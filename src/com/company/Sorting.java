e3b3eb9afde42c5402bad4784678d562aae4a68e
isaonaranjo <35385804+isaonaranjo@users.noreply.github.com>
2019-02-06 17:54:41 -0600

    Add files via upload

---
+++ src/com/company/Sorting.java
@@ -0,0 +1,47 @@
+/*
+* Author: Isabel Ortiz
+* Author: Diego Estrada
+* Clase que contiene los metodos para probar el ordenamiento de algoritmos.
+* */
+package com.company;
+
+
+public class Sorting {
+
+    /** Metodo de Gnome sort, lo que hace este metodo es revisar cada elemento de la lista que va a ser ordenada con el siguiente, intercambiándolos de posición si están en el orden equivocado.
+     * @param a array de los 3000 enteros generados aleatoriamente, que luego de la llamada quedan ordenados.
+     */
+    public static void GnomeSort(int[] a) {
+
+    }
+
+    /** Metodo de Merge sort, lo que este metodo hace es realizar comparaciones entre los elementos del array minimo.
+     * @param a array de los 3000 enteros generados aleatoriamente
+     * @param izq posicion en que esta el array
+     * @param m elemento central
+     * @param der posicion en que esta el array
+     */
+    public static void MergeSort(int[] a,int izq, int m, int der){
+
+    }
+
+    /** Metodo de Quick sort, en lo que consiste es en ir subdividiendo el array en arrays más pequeños, y ordenarlos
+     * @param a array de los 3000 enteros generados aleatoriamente
+     * @param inicio posicion inicial en el array
+     * @param fin posicion final en el array
+     */
+    public static void QuickSort (int[] a, int inicio, int fin){
+
+
+    }
+
+    /** Metodo RadixSort, este metodo ordena los elemetos del array de menor a mayor.
+     * @param a array de los 3000 enteros generados aleatoriamente
+     * @param n posicion del elemento que se desea
+     */
+    public static void RadixSort(int[] a, int n ){
+
+
+    }
+
+}
